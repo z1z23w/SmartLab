59a34ea0df17dc765b8ffa95042b5ca3ec35e824
2439451174@qq.com <2439451174@qq.com>
2026-01-10 22:29:26 +0800

    合并远程main分支

--- MyWeChat_Client/build/C_C_2026_01_10-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appwe_FILE /run/media/root/f6ad52dd-6a3e-40b7-9a1f-8323c318e456/大型软件开发/WeChat-main/MyWeChat_Client/build/C_C_2026_01_10-Debug/appwe)
-set(__QT_DEPLOY_TARGET_appwe_TYPE EXECUTABLE)
--- MyWeChat_Server/build/C_C_2026_01_10-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_WeChatServer_FILE /run/media/root/f6ad52dd-6a3e-40b7-9a1f-8323c318e456/大型软件开发/WeChat-main/MyWeChat_Server/build/C_C_2026_01_10-Debug/WeChatServer)
-set(__QT_DEPLOY_TARGET_WeChatServer_TYPE EXECUTABLE)
